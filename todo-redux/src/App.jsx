a066384f2d4579e3fe273d3bee5f12906079d2c1
Ashish <ashishsaud498@gmail.com>
2025-01-22 00:06:19 +0545

    todo app is made using react  redux toolkit

--- todo-redux/src/App.jsx
+++ todo-redux/src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
 
 const App = () => {
   return (
-    <div>App</div>
-    // 27: 00
+    <div className='flex flex-col justify-center items-center h-screen bg-zinc-900'>
+      <AddTodo />
+      <Todos />
+    </div>
   )
 }
 
--- todo-redux/src/features/todo/todoSlice.js
+++ todo-redux/src/features/todo/todoSlice.js
@@ -1,14 +1,27 @@
 import { createSlice, nanoid } from "@reduxjs/toolkit";
 
+// initial state (each slice)
 const initialState = {
-  todos: [{id: 1, text : 'helloworld'}]
+  todos: []
 }
 
 export const todoSlice = createSlice({
-  name : 'todo',
-  initialState ,
-  reducers : {
-    addTodo : () => {},
-    removeTpdp :() => {},
+  name: 'todo', // name of the slice
+  initialState,  // initial state (each slice)
+  reducers: {  //reducers (properties)
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        text: action.payload,
+      }
+      state.todos.push(todo)
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload)
+    },
   }
 })
+
+export const { addTodo, removeTodo } = todoSlice.actions
+
+export default todoSlice.reducer
